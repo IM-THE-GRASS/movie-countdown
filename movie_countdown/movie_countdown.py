175aa54bd63b893b093316a75deaaab2aba63de2
IM-THE-GRASS <thegrassmanreplit@gmail.com>
2024-08-16 03:48:35 -0600

    gets data from api now even tho it doesnt do much with that data also the countdown exists

--- movie_countdown/movie_countdown.py
+++ movie_countdown/movie_countdown.py
@@ -1,16 +1,50 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 import reflex as rx
+import requests
+import json
 
 from rxconfig import config
 
 
 class State(rx.State):
-    """The app state."""
+    times:dict[str, str] = {"Days":"20", "Hours":"11", "Minutes":"23", "Seconds":"34"}
+    overview:str
+    title:str
+    type:str
+    release_date:str
+    img_url:str
+    def get_data(self):
+        response = requests.get("https://whenisthenextmcufilm.com/api")
+        data = json.loads(response.text)
+        self.title = data["title"]
+        self.overview = data["overview"]
+        self.type = data["type"]
+    def update_time(self, _):
+        print("TIME MAYBE UPDATE")
 
-    ...
-
-
+def countdown_timer(info):
+    return rx.vstack(
+        rx.text(
+            info[1],
+            font_weight="bold",
+            line_height="7vh",
+            letter_spacing="-0.2vw",
+            font_size="13vh",
+            text_align="center",
+            font_family="Roboto"
+        ),
+        rx.text(
+            info[0],
+            font_weight="bold",
+            line_height="2.6vh",
+            letter_spacing="0vw",
+            font_size="4vh",
+            text_align="center",
+            font_family="Roboto",
+            width="100%"
+        )
+    )
 def index() -> rx.Component:
     # Welcome Page (Index)
     return rx.center(
@@ -28,38 +62,57 @@ def index() -> rx.Component:
                             rx.text(
                                 "This is the name of the movie",
                                 text_align="start",
-                                font_size="24px",
-                                letter_spacing="-2px",
-                                line_height="24px"
+                                font_size="2.612vh",
+                                letter_spacing="-0.1vw",
+                                line_height="2.612vh",
+                                font_family="Roboto"
                             ),
                             rx.text(
                                 "This is where the overview will go",
-                                font_size="14px",
-                                letter_spacing="0.25px",
-                                line_height="20px",
-                                color="E7E0EC"
+                                font_size="2vh",
+                                letter_spacing="1px",
+                                line_height="2.2vh",
+                                color="E7E0EC",
+                                font_family="Roboto",
+                                height="15vh"
                             )
                         ),
                         rx.box(
                             rx.text(
                                 "xxxx-xx-xx ・ Movie",
-                                font_size="14px",
-                                letter_spacing="0.25px",
-                                line_height="20px",
+                                font_size="2vh",
+                                letter_spacing="1px",
+                                line_height="2.2vh",
                                 text_align="end",
                                 color="E7E0EC",
-                                height="5.5vh"
+                                height="5.5vh",
+                                font_family="Roboto"
                             )
                         ),
                         justify_content="space-between",
                         width="42vw"
                     ),
+                    rx.hstack(
+                        rx.foreach(
+                            State.times,
+                            countdown_timer
+                        ),
+                        justify_content="space-around",
+                        width="42vw",
+                        height="100%"
+                    ),
                     width="42vw",
                     height="25vh"
-                )
+                ),
             ),
             width="42vw",
-            height="85vh"
+            height="95vh"
+        ),
+        rx.moment(
+            left="1000vw",
+            position="absolute",
+            interval=1000,
+            on_change=State.update_time()  
         ),
         width="100vw",
         height="100vh",
@@ -67,5 +120,9 @@ def index() -> rx.Component:
     )
 
 
-app = rx.App()
+app = rx.App(
+    stylesheets=[
+        "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap",
+    ],
+)
 app.add_page(index)
